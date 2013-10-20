2b8295872c8a76fa80eb91e5980d327e66caf036
sergey firsov <sfirsov1972@gmail.com>
2013-10-19 17:48:36 -0700

    wrong id.

--- Assignment_2.cpp
+++ Assignment_2.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int  main()
 {
    Name="Firsov";
-   int StuId=20092784, NumLet
+   int StuId=20092785, NumLet
    double Result;
    NumLet=count(Name);
    cout << StuId << " % " << "2 is " << NumLet << endl;
 //   cout << "Hello World!\n\n";
 //   cout << "My last name is Firsov" << endl;
-//   cout << "My student ID is 20092784" << endl << endl;
+//   cout << "My student ID is 20092785" << endl << endl;
 //   cout << "I noticed in the syllabus that we have 2 Modules and Lab assignment.\n"
 //        << "But for 1 week we don't have Lab assignment." << endl << endl;
    return 0;
@@ -19,7 +19,7 @@ int  main()
 Hello World!
 
 My last name is Firsov
-My student ID is 20092784
+My student ID is 20092785
 
 I noticed in the syllabus that we have 2 Modules and Lab assignment.
 But for 1 week we don't have Lab assignment.
