21cf865740222a02580e87cefb0938c13506e989
sergey firsov <sfirsov1972@gmail.com>
2013-10-19 17:44:53 -0700

    The original commit for the password project

---
+++ Assignment_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int  main()
+{
+   cout << "Hello World!\n\n";
+   cout << "My last name is Firsov" << endl;
+   cout << "My student ID is 20092784" << endl << endl;
+   cout << "I noticed in the syllabus that we have 2 Modules and Lab assignment.\n"
+        << "But for 1 week we don't have Lab assignment." << endl << endl;
+   return 0;
+}
+/*------output-------
+Hello World!
+
+My last name is Firsov
+My student ID is 20092784
+
+I noticed in the syllabus that we have 2 Modules and Lab assignment.
+But for 1 week we don't have Lab assignment.
+
+Press any key to continue . . .
+-------------------*/
---
+++ Assignment_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int  main()
+{
+   Name="Firsov";
+   int StuId=20092784, NumLet
+   double Result;
+   NumLet=count(Name);
+   cout << StuId << " % " << "2 is " << NumLet << endl;
+//   cout << "Hello World!\n\n";
+//   cout << "My last name is Firsov" << endl;
+//   cout << "My student ID is 20092784" << endl << endl;
+//   cout << "I noticed in the syllabus that we have 2 Modules and Lab assignment.\n"
+//        << "But for 1 week we don't have Lab assignment." << endl << endl;
+   return 0;
+}
+/*------output-------
+Hello World!
+
+My last name is Firsov
+My student ID is 20092784
+
+I noticed in the syllabus that we have 2 Modules and Lab assignment.
+But for 1 week we don't have Lab assignment.
+
+Press any key to continue . . .
+-------------------*/
